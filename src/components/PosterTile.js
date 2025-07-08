3d329dc17cbaf93cc93ef80163a2ac288941d2d8
Md Fazlur Rahman <mfzlur@gmail.com>
2025-07-08 16:26:35 +0530

    added postertile and removed youtube tile

---
+++ src/components/PosterTile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// 1. Import your static image from the assets folder
+import posterImage from '../assets/incleclub-poster.png';
+
+const PosterTile = () => {
+    return (
+        // 2. Use a different class for styling the full-width container
+        <div className="full-width-poster">
+            <img src={posterImage} alt="Promotional Poster" />
+        </div>
+    );
+};
+
+export default PosterTile;
